7f8353399286b633957fadb917d7e00317a52859
markAnthoner <marcoasp57@gmail.com>
2022-03-31 19:05:12 -0600

    Funciona el login con usuarios alumnos
    En este commit se tiene una conexión con la base de datos, y si usa el login para entrar, entra corectamente unicamente si ingresa el usuario y la contraseña.

--- Proyecto - sin video/Configuracion/config.php
+++ Proyecto - sin video/Configuracion/config.php
@@ -1,6 +1,6 @@
 <?php 
 
-	//Coexion con la base de datos
+	/*Coexion con la base de datos
 	$host = "localhost"; 
 	$dbname = "proyectocursos"; 
 	$username = "root"; 
@@ -10,5 +10,14 @@
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); } 
 	catch (PDOException $exception){ 
 		die("Connection error: " . $exception->getMessage()); 
-	} 
+	} */
+
+	//definimos constantes para hacer la conexion con la base de datos
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'proyectocursos');
+
+   //se abre una nueva conexion con el servidor de MySQL
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?> 
